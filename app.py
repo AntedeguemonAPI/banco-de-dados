23298cd2497dbf3b272e9f34df92ea5585361574
Jean Faria <111452998+jeejinf@users.noreply.github.com>
2025-05-26 00:15:49 -0300

    hotfix: atualizando endereço cors

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 
-CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:3001"}})
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}})
 
 
 # Inicialize a API Flask-RESTX
